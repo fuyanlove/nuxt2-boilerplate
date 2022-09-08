d5724e8c47be47878c8db4f8ddf19ef698d956cb
iRuxu <rx6@qq.com>
2022-09-08 08:18:46 +0800

    feat:api

--- settings.js
+++ settings.js
@@ -2,4 +2,12 @@ module.exports = {
     // SEO
     title: "title",
     description: "desc",
+
+    // URL
+    __cdn: "https://console.cnyixun.com/easticloud/",
+
+    // API
+    API:{
+        cms: "https://cms.easticloud.iruxu.com/",
+    }
 };
